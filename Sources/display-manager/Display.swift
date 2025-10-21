85315471db9fe6be753bc444fadf7505d3bd16ed
Frizlab <fload@me.com>
2025-10-21 23:33:23 +0200

    Add a method to get the IOServiceInfoDictionary of a display

--- Sources/display-manager/Display.swift
+++ Sources/display-manager/Display.swift
@@ -1,5 +1,6 @@
 import CoreGraphics
 import Foundation
+import IOKit
 
 import Logging
 
@@ -96,6 +97,47 @@ struct Display {
 			throw Err.internalError(message: "Invalid display ID.")
 		}
 		return mode
+	}
+	
+	/* Adapted from <https://medium.com/@zpcat/how-to-get-displays-device-name-by-iokit-in-mac-os-x-f91f42e8955>. */
+	func getIOServiceInfoDictionary() throws -> [String: AnyObject] {
+		var serialPortIterator: io_iterator_t = 0
+		let matching = IOServiceMatching("IODisplayConnect")
+		guard IOServiceGetMatchingServices(kIOMasterPortDefault, matching, &serialPortIterator) == KERN_SUCCESS,
+				serialPortIterator != 0
+		else {
+			throw Err.internalError(message: "Cannot get serial port iterator.")
+		}
+		defer {IOObjectRelease(serialPortIterator)}
+		
+		struct IOIterator : IteratorProtocol {
+			let serialPortIterator: io_iterator_t
+			func next() -> io_service_t? {
+				let service = IOIteratorNext(serialPortIterator)
+				guard service != 0 else {
+					return nil
+				}
+				return service
+			}
+		}
+		
+		let iterator = IOIterator(serialPortIterator: serialPortIterator)
+		while let ioService = iterator.next() {
+			guard let info = IODisplayCreateInfoDictionary(ioService, UInt32(kIODisplayOnlyPreferredName)).takeRetainedValue() as? [String: AnyObject] else {
+				Logger(label: "CGDirectDisplayID+Utils").warning("Failed retrieving IO info dictionary for IO service. Skipping this service.")
+				continue
+			}
+			
+			/* Note: We take the first that matches w/o checking if another would (which should never happen). */
+			if CGDisplayVendorNumber(id) == info[kDisplayVendorID] as? UInt32,
+				CGDisplayModelNumber(id)  == info[kDisplayProductID] as? UInt32,
+				CGDisplaySerialNumber(id) == info[kDisplaySerialNumber] as? UInt32 ?? 0
+			{
+				return info
+			}
+		}
+		
+		throw Err.internalError(message: "IOService not found for display ID.")
 	}
 	
 	private static func getAllDisplayIDs() throws -> [CGDirectDisplayID] {
